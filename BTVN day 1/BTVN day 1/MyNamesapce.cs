b24262b583b6fa08e575b46d7493a2ba7fb41c32
luuhuythanh0111 <hongnhungle491@gmail.com>
2023-04-12 22:16:53 +0700

    Them 1 cai gi day

---
+++ BTVN day 1/BTVN day 1/MyNamesapce.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using static  System.Console;
+using MyNameSpaceChild;            
+
+
+namespace MyNameSpace
+{
+    public class MyNamesapce
+    {
+        public static void Main(string[] args)
+        {
+            Test1 t1 = new Test1();     //khai báo t1 là tên biến mới như string name = ...
+            t1.Hello();                 //gọi biến: tên biến.hàm. Biến cho ra màn hình câu lệnh như bên Test1
+
+            Test2 t2 = new Test2();
+            t2.Hello();
+            //WriteLine("Hello, I am Jesus Christ!");
+        }
+    }
+}
---
+++ BTVN day 1/BTVN day 1/MyNamespaceChild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MyNameSpaceChild
+{
+    public class Test1
+    {
+        public void Hello() //Hello() là hàm
+        {
+            WriteLine("Hello, how are you bro?");
+        }
+        
+    }
+    public class Test2
+    {
+       public void Hello()
+        {
+            WriteLine("I am fine, thanks!");
+        }
+    }
+}
+                   
